0713ae71081b2b85e66b6b99f55c6d5421646a25
PaiIan722 <144758494+PaiIan722@users.noreply.github.com>
2024-11-07 16:29:42 +0800

    week09

---
+++ week09/week09-1.c
@@ -0,0 +1,15 @@
+//week09-1.c 學計畫第1題
+//LeetCode 1672. Richest Customer Wealth
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
+    int M = accountsSize;
+    int N = accountsColSize[0];
+    int ans = 0;
+    for(int i=0;i<M;i++){
+        int total=0;
+        for(int j=0;j<N;j++){
+            total += accounts[i][j];
+        }
+        if(total>ans) ans=total;
+    }
+    return ans;
+}
