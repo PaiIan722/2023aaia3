0e0e66c670b675a74da5712578552efa08802838
PaiIan722 <144758494+PaiIan722@users.noreply.github.com>
2024-12-26 16:24:35 +0800

    week16

---
+++ week16/week16-2a.cpp
@@ -0,0 +1,8 @@
+//week16-2a.cpp 學習計畫
+//50. Pow(x, n)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);//cheat
+    }
+};
---
+++ week16/week16-2b.cpp
@@ -0,0 +1,16 @@
+//week16-2b.cpp 學習計畫
+//50. Pow(x, n)
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1;
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        for(int i=0;i<n;i++){
+            ans*=x;
+        }
+        return ans;
+    }
+};
